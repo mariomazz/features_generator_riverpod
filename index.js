a349e5fe69613f173e1a705d409d6393cf3e2ab8
mario <mario.mazzarelli@asfweb.it>
2024-02-18 00:52:45 +0100

    DONE

--- index.js
+++ index.js
@@ -65,6 +65,14 @@ function startFeatureCreation() {
             path = `${process.cwd()}${inputDir}${featureName}`
         }
 
+        let parsedfeatureName = "";
+
+        featureName.split("_").forEach((data, i) => {
+            parsedfeatureName += capitalize(data);
+        });
+
+        parsedfeatureName = removeCapitalize(parsedfeatureName);
+
         console.log(`Writing feature ${featureName} ...`);
         try {
             features.forEach((feature, i) => {
@@ -73,8 +81,8 @@ function startFeatureCreation() {
                 switch (feature.key) {
                     case "ui":
                         if (createPresentation) {
-                            writeFile(`${path}/${feature.value}`, `${featureName}_screen.dart`, screenDartData(featureName));
-                            writeFile(`${path}/${feature.value}`, `${featureName}_screen_controller.dart`, screenControllerDartData(featureName));
+                            writeFile(`${path}/${feature.value}`, `${featureName}_screen.dart`, screenDartData(featureName, parsedfeatureName));
+                            writeFile(`${path}/${feature.value}`, `${featureName}_screen_controller.dart`, screenControllerDartData(featureName, parsedfeatureName));
                             if (presentationOnly) {
                                 let feature = features.find((feature) => {
                                     return feature.key == "data";
@@ -88,16 +96,16 @@ function startFeatureCreation() {
 
                     case "bl":
                         if (createApplication) {
-                            writeFile(`${path}/${feature.value}`, `${featureName}_service.dart`, serviceDartData(featureName));
+                            writeFile(`${path}/${feature.value}`, `${featureName}_service.dart`, serviceDartData(featureName, parsedfeatureName)); // TODO --
                         }
                         break;
 
                     case "data":
 
                         if (createData) {
-                            writeFile(`${path}/${feature.value}`, `provider.dart`, providerDartData(featureName));
-                            writeFile(`${path}/${feature.value}`, `${featureName}_repository.dart`, repositoryDartData(featureName));
-                            writeFile(`${path}/${feature.value}`, `${featureName}_repository.interface.dart`, repositoryInterfaceDartData(featureName));
+                            writeFile(`${path}/${feature.value}`, `provider.dart`, providerDartData(featureName, parsedfeatureName));// TODO --
+                            writeFile(`${path}/${feature.value}`, `${featureName}_repository.dart`, repositoryDartData(featureName, parsedfeatureName));// TODO --
+                            writeFile(`${path}/${feature.value}`, `${featureName}_repository.interface.dart`, repositoryInterfaceDartData(featureName, parsedfeatureName));// TODO --
                             writeFile(`${path}/${feature.value}/model`, `model.dart`, modelDartData());
                         }
                         break;
@@ -245,33 +253,32 @@ class Model {
     `;
 }
 
-function providerDartData(featureName) {
-
-    let capitalizedFeatureName = capitalize(featureName);
+function providerDartData(snakedFeatureName, camelCaseFeatureName) {
+    let capitalizedCamelCaseFeatureName = capitalize(camelCaseFeatureName);
 
     return `
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import '${featureName}_repository.dart';
-import '${featureName}_repository.interface.dart';
+import '${snakedFeatureName}_repository.dart';
+import '${snakedFeatureName}_repository.interface.dart';
 part 'provider.g.dart';
 
 @Riverpod(keepAlive: true)
-I${capitalizedFeatureName}Repository ${featureName}Repository(${capitalizedFeatureName}RepositoryRef ref) {
-    return const ${capitalizedFeatureName}Repository();
+I${capitalizedCamelCaseFeatureName}Repository ${camelCaseFeatureName}Repository(${capitalizedCamelCaseFeatureName}RepositoryRef ref) {
+    return const ${capitalizedCamelCaseFeatureName}Repository();
 }
     `;
 
 }
 
 
-function repositoryDartData(featureName) {
-    let capitalizedFeatureName = capitalize(featureName);
+function repositoryDartData(snakedFeatureName, camelCaseFeatureName) {
+    let capitalizedCamelCaseFeatureName = capitalize(camelCaseFeatureName);
     return `
 import 'model/model.dart';
-import '${featureName}_repository.interface.dart';
+import '${snakedFeatureName}_repository.interface.dart';
 
-class ${capitalizedFeatureName}Repository implements I${capitalizedFeatureName}Repository {
-    const ${capitalizedFeatureName}Repository();
+class ${capitalizedCamelCaseFeatureName}Repository implements I${capitalizedCamelCaseFeatureName}Repository {
+    const ${capitalizedCamelCaseFeatureName}Repository();
     @override
     Future<Model> load() async {
         return const Model();
@@ -280,60 +287,60 @@ class ${capitalizedFeatureName}Repository implements I${capitalizedFeatureName}R
     `;
 }
 
-function repositoryInterfaceDartData(featureName) {
-    let capitalizedFeatureName = capitalize(featureName);
+function repositoryInterfaceDartData(snakedFeatureName, camelCaseFeatureName) {
+    let capitalizedCamelCaseFeatureName = capitalize(camelCaseFeatureName);
 
     return `
 import 'model/model.dart';
 
-abstract class I${capitalizedFeatureName}Repository {
+abstract class I${capitalizedCamelCaseFeatureName}Repository {
     Future<Model> load();
 }
     `;
 
 }
 
-function serviceDartData(featureName) {
-    let capitalizedFeatureName = capitalize(featureName);
+function serviceDartData(snakedFeatureName, camelCaseFeatureName) {
+    let capitalizedCamelCaseFeatureName = capitalize(camelCaseFeatureName);
 
     return `
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import '../data/model/model.dart';
-import '../data/${featureName}_repository.interface.dart';
+import '../data/${snakedFeatureName}_repository.interface.dart';
 import '../data/provider.dart';
-part '${featureName}_service.g.dart';
+part '${snakedFeatureName}_service.g.dart';
 
-class ${capitalizedFeatureName}Service {
-  final I${capitalizedFeatureName}Repository _repo;
-  const ${capitalizedFeatureName}Service(this._repo);
+class ${capitalizedCamelCaseFeatureName}Service {
+  final I${capitalizedCamelCaseFeatureName}Repository _repo;
+  const ${capitalizedCamelCaseFeatureName}Service(this._repo);
   Future<Model> load() async {
     return _repo.load();
   }
 }
 
 @Riverpod(keepAlive: true)
-${capitalizedFeatureName}Service ${featureName}Service(${capitalizedFeatureName}ServiceRef ref) {
-  final repo = ref.read(${featureName}RepositoryProvider);
-  return ${capitalizedFeatureName}Service(repo);
+${capitalizedCamelCaseFeatureName}Service ${camelCaseFeatureName}Service(${capitalizedCamelCaseFeatureName}ServiceRef ref) {
+  final repo = ref.read(${camelCaseFeatureName}RepositoryProvider);
+  return ${capitalizedCamelCaseFeatureName}Service(repo);
 }
     `;
 
 }
 
-function screenDartData(featureName) {
-    let capitalizedFeatureName = capitalize(featureName);
+function screenDartData(snakedFeatureName, camelCaseFeatureName) {
+    let capitalizedCamelCaseFeatureName = capitalize(camelCaseFeatureName);
     return `
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '${featureName}_screen_controller.dart';
+import '${snakedFeatureName}_screen_controller.dart';
 
-class ${capitalizedFeatureName}Screen extends ConsumerWidget {
-    const ${capitalizedFeatureName}Screen({super.key});
+class ${capitalizedCamelCaseFeatureName}Screen extends ConsumerWidget {
+    const ${capitalizedCamelCaseFeatureName}Screen({super.key});
 
     @override
     Widget build(BuildContext context, WidgetRef ref) {
         // ignore: unused_local_variable
-        final viewModelState = ref.watch(${featureName}ScreenControllerProvider);
+        final viewModelState = ref.watch(${camelCaseFeatureName}ScreenControllerProvider);
         return const Scaffold();
     }
 }
@@ -341,23 +348,23 @@ class ${capitalizedFeatureName}Screen extends ConsumerWidget {
 }
 
 
-function screenControllerDartData(featureName) {
-    let capitalizedFeatureName = capitalize(featureName);
+function screenControllerDartData(snakedFeatureName, camelCaseFeatureName) {
+    let capitalizedCamelCaseFeatureName = capitalize(camelCaseFeatureName);
     return `
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import '../data/model/model.dart';
-part '${featureName}_screen_controller.g.dart';
+part '${snakedFeatureName}_screen_controller.g.dart';
 
 @Riverpod(keepAlive: false)
-class ${capitalizedFeatureName}ScreenController extends _$${capitalizedFeatureName}ScreenController {
-    final _initialState = const ${capitalizedFeatureName}ScreenControllerState.empty();
-    // late final _service = ref.read(${featureName}ServiceProvider);
+class ${capitalizedCamelCaseFeatureName}ScreenController extends _$${capitalizedCamelCaseFeatureName}ScreenController {
+    final _initialState = const ${capitalizedCamelCaseFeatureName}ScreenControllerState.empty();
+    // late final _service = ref.read(${camelCaseFeatureName}ServiceProvider);
 
     // @override
-    // ${capitalizedFeatureName}ScreenControllerState state => stateOrNull ?? _initialState;
+    // ${capitalizedCamelCaseFeatureName}ScreenControllerState get state => stateOrNull ?? _initialState;
 
     @override
-    ${capitalizedFeatureName}ScreenControllerState build() {
+    ${capitalizedCamelCaseFeatureName}ScreenControllerState build() {
         _initialize();
         _disposing();
         return _initialState;
@@ -372,16 +379,16 @@ class ${capitalizedFeatureName}ScreenController extends _$${capitalizedFeatureNa
     }
 }
 
-class ${capitalizedFeatureName}ScreenControllerState {
+class ${capitalizedCamelCaseFeatureName}ScreenControllerState {
     final AsyncValue<Model> data;
 
-    const ${capitalizedFeatureName}ScreenControllerState({required this.data});
-    const ${capitalizedFeatureName}ScreenControllerState.empty({this.data = const AsyncLoading()});
+    const ${capitalizedCamelCaseFeatureName}ScreenControllerState({required this.data});
+    const ${capitalizedCamelCaseFeatureName}ScreenControllerState.empty({this.data = const AsyncLoading()});
 
-    ${capitalizedFeatureName}ScreenControllerState copyWith({
+    ${capitalizedCamelCaseFeatureName}ScreenControllerState copyWith({
         AsyncValue<Model>? data,
     }) {
-        return ${capitalizedFeatureName}ScreenControllerState(
+        return ${capitalizedCamelCaseFeatureName}ScreenControllerState(
             data: data ?? this.data,
         );
     }
@@ -427,7 +434,7 @@ class ${capitalizedCamelCaseFeatureName}Controller extends _$${capitalizedCamelC
     // late final _service = ref.read(${camelCaseFeatureName}ServiceProvider);
 
     // @override
-    // ${capitalizedCamelCaseFeatureName}ControllerState state => stateOrNull ?? _initialState;
+    // ${capitalizedCamelCaseFeatureName}ControllerState get state => stateOrNull ?? _initialState;
 
     @override
     ${capitalizedCamelCaseFeatureName}ControllerState build() {
